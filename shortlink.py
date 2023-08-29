4c61bc7760e29cc5662ec5f3093b13cca71b2fa4
Acckerman <109450912+Acckerman@users.noreply.github.com>
2023-08-29 10:57:56 +0530

    Update shortlink.py

--- shortlink.py
+++ shortlink.py
@@ -3,7 +3,7 @@ import asyncio
 from os import getenv, environ
 from asyncio import TimeoutError
 from shortzy import Shortzy
-from Adarsh.vars import Var
+from Config.py import config.py
 
 SHORTENER_API = str(getenv('SHORTENER_API', 'e0867ce24e2238645541bf7651be2217b4cd5dd1'))
 SHORTENER_WEBSITE = str(getenv('SHORTENER_WEBSITE', 'shorturllink.in'))
