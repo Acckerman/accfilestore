6199dd1b7b97810e03dc15d0d46910123884735b
Acckerman <109450912+Acckerman@users.noreply.github.com>
2023-08-29 11:14:06 +0530

    Update shortlink.py

--- shortlink.py
+++ shortlink.py
@@ -5,8 +5,8 @@ from asyncio import TimeoutError
 from shortzy import Shortzy
 from Config.py import config.py
 
-SHORTENER_API = os.environ.get('SHORTENER_API', 'e0867ce24e2238645541bf7651be2217b4cd5dd1'))
-SHORTENER_WEBSITE = os.environ.get('SHORTENER_WEBSITE', 'shorturllink.in'))
+SHORTENER_API = os.environ.get('SHORTENER_API', 'e0867ce24e2238645541bf7651be2217b4cd5dd1')
+SHORTENER_WEBSITE = os.environ.get('SHORTENER_WEBSITE', 'shorturllink.in')
 
 
 shortzy = Shortzy(SHORTENER_API, SHORTENER_WEBSITE)
